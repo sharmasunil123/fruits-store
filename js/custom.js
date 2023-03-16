cabf80342102e15a593623db57d871f502104f5b
Sunil Sharma <sunilsharma7583@gmail.com>
2023-03-16 12:55:43 +0530

    html layout

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".slider").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1300,
+      arrows: false,
+         dots: false,
+    });
+  });
